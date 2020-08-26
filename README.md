80808ce6ba0666091876cec8feefbdc71e69b4df
David Zech <david@davidzech.com>
2020-08-26 01:00:57 -0700

    Update help text

--- README.md
+++ README.md
@@ -9,7 +9,8 @@ Fearlessly present your command line demos with precision. Inspired by [Hackerty
 ```console
 → autopilot --help
 A tool that automatically types your script into a new instance of your shell via PTY. 
-                        When you press a key, the next character of your script is entered into the PTY. Newlines are only consumed when you press [enter].
+Pressing a key causes the next character of your script to be entered into the PTY. 
+Newlines are only consumed when you press [enter].
 
 Usage:
   autopilot [flags] script
@@ -17,6 +18,7 @@ Usage:
 Flags:
   -h, --help           help for autopilot
   -s, --shell string   Shell to use for execution (default "/bin/zsh")
+
 ```
 
 ## Hashbang
--- cmd/cmd.go
+++ cmd/cmd.go
@@ -10,12 +10,15 @@ import (
 
 var shell string
 
+const long = `A tool that automatically types your script into a new instance of your shell via PTY. 
+Pressing a key causes the next character of your script is entered into the PTY. 
+Newlines are only consumed when you press [enter].`
+
 var rootCmd = &cobra.Command{
 	Use:   "autopilot [flags] script",
 	Short: "A tool that automatically types your script on keypress",
-	Long: `A tool that automatically types your script into a new instance of your shell via PTY. 
-			When you press a key, the next character of your script is entered into the PTY. Newlines are only consumed when you press [enter].`,
-	Args: cobra.ExactArgs(1),
+	Long:  long,
+	Args:  cobra.ExactArgs(1),
 	RunE: func(_ *cobra.Command, args []string) error {
 		file, err := os.Open(args[0])
 		if err != nil {
